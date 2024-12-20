b17097553f6cb3f49da27cb4a082c1756709a906
Keegan Bruer <keegan.bruer@gmail.com>
2024-12-20 05:50:07 -0500

    Clean House

--- src/Components/Deepdive/Authors/Keegan/index.tsx
+++
@@ -1,8 +0,0 @@
-import { getPostData, getPostMeta } from "@/hooks/getPostData";
-import Content from "./content"
-
-export default async (props: { }) => {
-    let posts = await getPostData(undefined, {visibilityOverride:true});
-    return <Content posts={posts} />
-
-}
